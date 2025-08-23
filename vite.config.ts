068d6acc82b814e06ac6f819784885bed9701811
Abhijay 3007 <abhijayr05@gmail.com>
2025-08-23 14:10:18 +0530

    Server Changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
+    allowedHosts: [
+      'codefury-hackathon.onrender.com' // ✅ whitelist your host
+    ]
   },
   plugins: [
     react(),
