32401cc69eaf93b4ffcc31802cfd4f9cc8034565
Abhijay 3007 <abhijayr05@gmail.com>
2025-08-23 14:59:09 +0530

    main.tsx changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import App from './App.tsx'
+import App from './App.tsx' // 👈 important
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+   <App/>
   </React.StrictMode>
 )
 
